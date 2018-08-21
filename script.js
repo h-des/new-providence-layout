34973936305133cea733e2f709423e890a8dcbb9
h-des <patryk.holysz@gmail.com>
2018-08-21 20:02:52 +0200

    add script for carousel images animations

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const opinions = document.querySelectorAll(".carousel__opinion");
+const avatars = document.querySelectorAll(".carousel__avatar");
+const carouselLeftButton = document.querySelector(".carousel__button--left");
+const carouselRightButton = document.querySelector(".carousel__button--right");
+
+
+carouselLeftButton.addEventListener('click', () => {
+    avatarsMove(); 
+    carouselMove();
+  }
+);
 
 
 let carouselVisibleOpinionID = 0;
+let avatarMainID = 1;
 
 setInterval(() => {
   carouselMove();
+  avatarsMove();
 }, 2000);
 
 const VISIBLE_CLASS = 'carousel__opinion--visible';
 const HIDDEN_CLASS = 'carousel__opinion--hidden';
 const HIDING_CLASS = 'carousel__opinion--hiding';
 const SHOWING_CLASS = 'carousel__opinion--showing';
+
+const AVATAR_LEFT_CLASS = 'carousel__avatar--left';
+const AVATAR_RIGHT_CLASS = 'carousel__avatar--right';
+const AVATAR_MAIN_CLASS = 'carousel__avatar--main';
 
 function carouselMove () {
   let opinionListLength = opinions.length;
@@ -40,4 +56,37 @@ function showOpinion (opinion) {
 function hideOpinion (opinion) {
   opinion.classList.remove(VISIBLE_CLASS);
   opinion.classList.add(HIDDEN_CLASS);
+}
+
+function avatarsMove () {
+  const length = avatars.length;
+  let centerID = avatarMainID;
+  let leftID = null, rightID = null;
+  if ( avatarMainID - 1 < 0) {
+    leftID = length - 1;
+  } else {
+    leftID = avatarMainID - 1;
+  }
+
+  if( avatarMainID + 1 >= length) {
+    rightID = 0;
+  } else {
+    rightID = avatarMainID +1;
+  }
+
+  avatars.forEach( e => {
+    e.classList.remove(AVATAR_LEFT_CLASS);
+    e.classList.remove(AVATAR_RIGHT_CLASS);
+    e.classList.remove(AVATAR_MAIN_CLASS);
+  })
+
+  avatarsList = [...avatars];
+
+  
+  avatarsList[leftID].classList.add(AVATAR_LEFT_CLASS);
+  avatarsList[rightID].classList.add(AVATAR_RIGHT_CLASS);
+  avatarsList[centerID].classList.add(AVATAR_MAIN_CLASS);
+
+  avatarMainID += 1;
+  avatarMainID = avatarMainID % length;
 }
